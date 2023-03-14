8e802a3f1675e37d4010e8d1ee6fd38ec60ba078
Henry.Huang <hhglory@outlook.com>
2023-03-14 17:50:03 +0800

    Deploy website - based on 8138e0d76226c297622e8f8cf49eb2e5f8b8655c

--- assets/js/3da1bc2b.867c8389.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[2180],{7273:e=>{e.exports=JSON.parse('{"title":"Mall","description":"B2B2C \u5546\u57ce","slug":"/category/mall-1","permalink":"/zeromall/docs/category/mall-1","navigation":{"next":{"title":"\u7b80\u4ecb","permalink":"/zeromall/docs/biz/mall/intro"}}}')}}]);
